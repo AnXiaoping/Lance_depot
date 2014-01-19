2b66fe72beb34ac33eb9f4ee0feb9c039b34e77d
AnXiaoping <ping.an@xingxinghuo.com>
2014-01-19 17:40:51 +0800

    chapter 9

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -4,6 +4,10 @@ class StoreControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    assert_select '#columns #side a', :minimum => 3
+    assert_select '#main .entry', 2
+    assert_select 'h3', 'Lance\'s test'
+    assert_select '.price', /\$[.\d]+\.\d\d/
   end
 
 end
