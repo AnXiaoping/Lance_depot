9e412c8b4279cf4b9a430fc41e6a9f043a19a57b
AnXiaoping <ping.an@xingxinghuo.com>
2014-02-17 19:04:35 +0800

    chapter 11 _2

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -25,13 +25,13 @@ class LineItemsController < ApplicationController
   # POST /line_items.json
   def create
     @cart = current_cart
-    product = Product.find(params[:product_id])
     # @line_item = LineItem.new(line_item_params)
-    @line_item = @cart.add_product(product.id)
+    @line_item = @cart.add_product(params[:product_id])
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
+        format.html { redirect_to(store_url) }
+        format.js {@current_item = @line_item}
         format.json { render action: 'show', status: :created, location: @line_item }
       else
         format.html { render action: 'new' }
